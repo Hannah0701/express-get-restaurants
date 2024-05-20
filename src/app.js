d80b20152eaf33fed6184860913b3024e1c355e1
Hannah Phillips <hannah.phillips@simplybusiness.co.uk>
2024-05-20 13:57:52 +0100

    added updates to make server call

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
 
+app.get("/restaurants", async (req, res) => {
+//     Get all restaurants via the Restaurant.findAll() method within the route.
+// Remember to use async and await
+    const restaurants = await Restaurant.findAll({});
+        // Send the restaurants as a JSON Response (res.json())
+    res.json(restaurants);
+});
 
 
+// app.get("/restaurants", (req, res) => {
+// //     Get all restaurants via the Restaurant.findAll() method within the route.
+// // Remember to use async and await
+//     Restaurant.findAll().then((restaurants) => {
+//         // Send the restaurants as a JSON Response (res.json())
+//         res.json(restaurants);
+//     });
+// });
 
 module.exports = app;
