f818243d3bcbb80852b0dcbf59388cc71326efab
Hannah Phillips <hannah.phillips@simplybusiness.co.uk>
2024-05-24 09:46:45 +0100

    updated my own test to use a different association method in test and added some commented suggestions from jordan

--- db/connection.js
+++ db/connection.js
@@ -7,4 +7,17 @@ const db = new Sequelize({
     logging: false
 });
 
+// console.log(process.env.NODE_ENV);
+
+// let db;
+// if (process.env.NODE_ENV === "test") {
+//     db = new Sequelize("sqlite::memory:", { logging: false });
+// } else {
+//     db = new Sequelize({
+//     dialect: 'sqlite',
+//     storage: path.join(__dirname, 'db.sqlite'),
+//     logging: false,
+//     });
+// };
+
 module.exports = db;
--- index.test.js
+++ index.test.js
@@ -4,9 +4,14 @@ execSync('npm run seed');
 
 const request = require('supertest');
 const app = require('./src/app.js');
+// const db = require('./db/connection');
 const { Restaurant, Menu, Item } = require('./models/index');
 
 describe('/restaurants endpoint', () => {
+    // beforeEach(async () => {
+    //     await db.sync({force: true});
+    // });
+
     test("status is 200", async () => {
         const restaurants = await request(app).get("/restaurants");
         expect(restaurants.status).toBe(200);
@@ -50,6 +55,92 @@ describe('/restaurants endpoint', () => {
             location: 'Texas',
             cuisine: 'FastFood'
         });
+    });
+
+    // test("GET /restaurants/:id should return a single restaurant with menu and items", async () => {
+    //     await Restaurant.create(
+    //       {
+    //         name: 'Tapas Molecular Bar',
+    //         location: 'Tokyo',
+    //         cuisine: 'Japanese',
+    //         menus: [
+    //             {
+    //             title: 'Main Menu',
+    //             items: [
+    //                 {
+    //                 name: 'Sushi',
+    //                 image: 'test.jpg',
+    //                 price: 15.99,
+    //                 vegetarian: false
+    //                 },
+    //               ],
+    //             },
+    //         ],
+    //       },
+    //     );
+    //     const restaurant = await request(app).get("/restaurants/4");
+    //     expect(restaurant.body).toMatchObject({
+    //         name: 'Tapas Molecular Bar',
+    //         location: 'Tokyo',
+    //         cuisine: 'Japanese',
+    //         menus: [
+    //             {
+    //                 title: 'Main Menu',
+    //                 items: [
+    //                     {
+    //                         name: 'Sushi',
+    //                         image: 'test.jpg',
+    //                         price: 15.99,
+    //                         vegetarian: false
+    //                     },
+    //                 ],
+    //             },
+    //         ],
+    //     });
+    // });
+
+    test("GET /restaurants/:id should return a single restaurant with menu and items", async () => {
+        const restaurant = await Restaurant.create({
+              name: 'Tapas Molecular Bar',
+              location: 'Tokyo',
+              cuisine: 'Japanese',
+        });
+        const menu = await restaurant.createMenu({
+            title: 'Main Menu'
+        });
+        await menu.createItem({
+                name: 'Sushi',
+                image: 'test.jpg',
+                price: 15.99,
+                vegetarian: false
+        });
+        const menus = await restaurant.getMenus();
+        const items = await menu.getItems();
+
+        expect(restaurant).toMatchObject({
+            name: 'Tapas Molecular Bar',
+            location: 'Tokyo',
+            cuisine: 'Japanese'
+        });
+        expect(menus).toMatchObject([
+            {
+                title: 'Main Menu',
+                restaurantId: 4
+            }
+        ]);
+        expect(items).toMatchObject([
+            {
+                name: 'Sushi',
+                image: 'test.jpg',
+                price: 15.99,
+                vegetarian: false,
+                menu_items: {
+                    itemId: 4,
+                    menuId: 4,
+                }
+            }
+        ]);
+
     });
 
     test("POST should add a new restaurant", async () => {
